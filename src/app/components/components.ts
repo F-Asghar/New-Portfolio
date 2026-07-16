64395d191e0221983c87d58693f034b1ecae80d0
ferazasghar6-max <ferazasghar6@gmail.com>
2026-07-16 17:09:27 +0200

    new structure and background implemented

--- src/app/components/components.ts
+++ src/app/components/components.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../shared/header-component/header-component';
 import { AboveTheFoldComponent } from './above-the-fold-component/above-the-fold-component';
 import { AboutMeComponent } from './about-me-component/about-me-component';
 import { MySkillsComponent } from './my-skills-component/my-skills-component';
 import { PortfolioComponent } from './portfolio-component/portfolio-component';
 import { ReferenceComponent } from './reference-component/reference-component';
 import { SayHiComponent } from './say-hi-component/say-hi-component';
-import { FooterComponent } from '../shared/footer-component/footer-component';
+
 
 @Component({
   selector: 'app-components',
