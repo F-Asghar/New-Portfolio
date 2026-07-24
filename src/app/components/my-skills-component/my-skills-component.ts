0a0ff1a6d931aafd0572791984f5e4b1d7263c72
ferazasghar6-max <ferazasghar6@gmail.com>
2026-07-24 13:49:24 +0200

    refactor(skills): simplify HTML template and move skill data to TS

--- src/app/components/my-skills-component/my-skills-component.ts
+++ src/app/components/my-skills-component/my-skills-component.ts
@@ -1,6 +1,13 @@
 import { Component } from '@angular/core';
 import { TranslatePipe } from '@ngx-translate/core';
 
+interface Skill {
+  name: string;
+  icon: string;
+  isAnimating?: boolean;
+}
+
+
 @Component({
   selector: 'app-my-skills-component',
   imports: [TranslatePipe],
@@ -9,13 +16,19 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 
 export class MySkillsComponent {
-  isLightMode: boolean = localStorage.getItem('isLightMode') === 'true';
-  isAnimAngular = false;
-  isAnimTs = false;
-  isAnimJs = false;
-  isAnimHtml = false;
-  isAnimCss = false;
-  isAnimGit = false;
-  isAnimRest = false;
-  isAnimEdu = false;
+  isAnimEducation = false;
+
+// In deiner Component-Klasse:
+skills: Skill[] = [
+  { name: 'Angular', icon: 'assets/icons/angular.png' },
+  { name: 'TypeScript', icon: 'assets/icons/typescript.png' },
+  { name: 'JavaScript', icon: 'assets/icons/javascript.png' },
+  { name: 'HTML', icon: 'assets/icons/html.png' },
+  { name: 'CSS', icon: 'assets/icons/css.png' },
+  { name: 'Git', icon: 'assets/icons/git.png' },
+  { name: 'REST-API', icon: 'assets/icons/rest-api.png' },
+  { name: 'Scrum', icon: 'assets/icons/scrum.png' },
+  { name: 'Supabase', icon: 'assets/icons/supabase.png' }
+];
+
 }
