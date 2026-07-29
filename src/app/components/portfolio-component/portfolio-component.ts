639253f827dd67d9d7eea2dce1a493aeb565d291
ferazasghar6-max <ferazasghar6@gmail.com>
2026-07-29 18:52:31 +0200

    feat(portfolio): add intersection observer and refine animations

--- src/app/components/portfolio-component/portfolio-component.ts
+++ src/app/components/portfolio-component/portfolio-component.ts
@@ -8,5 +8,27 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './portfolio-component.scss',
 })
 export class PortfolioComponent {
+  observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        const imageWrapper = entry.target.closest('.project')?.querySelector('.project-immage-wrapper');
 
+        if (entry.isIntersecting) {
+
+          imageWrapper?.classList.add('show');
+
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+          imageWrapper?.classList.remove('show');
+        }
+      });
+    },
+    { rootMargin: '0px 0px -120px 0px'},
+  );
+
+  ngAfterViewInit() {
+    const projects = Array.from(document.querySelectorAll('.project-content-wrapper'));
+    projects.forEach((element) => this.observer.observe(element));
+  }
 }
